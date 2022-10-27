ca6f2050b5977ce4a775b1864c2a118cdce4f66d
Егор Архипцев <e.arkhiptsev@gmail.com>
2022-10-28 00:18:11 +0300

    final fixed

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth import views
+
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
