f1fd0ab0b66657df8cd432971fda8177b2e8cb56
Егор Архипцев <e.arkhiptsev@gmail.com>
2022-10-29 19:37:39 +0300

    fix after review 2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,29 +5,29 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200, verbose_name="Заголовок",)
+    title = models.CharField(max_length=200, verbose_name='Заголовок',)
     slug = models.SlugField(unique=True)
-    description = models.TextField(verbose_name="Описание")
+    description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = "Группа"
-        verbose_name_plural = "Группы"
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
 
 
 class Post(models.Model):
-    text = models.TextField(verbose_name="Текст",)
+    text = models.TextField(verbose_name='Текст',)
     pub_date = models.DateTimeField(
         auto_now_add=True,
-        verbose_name="Дата публикации",
+        verbose_name='Дата публикации',
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='posts',
-        verbose_name="Автор"
+        verbose_name='Автор'
     )
     group = models.ForeignKey(
         Group,
@@ -35,10 +35,10 @@ class Post(models.Model):
         null=True,
         on_delete=models.SET_NULL,
         related_name='posts',
-        verbose_name="Группа",
+        verbose_name='Группа',
     )
 
     class Meta:
         ordering = ('-pub_date',)
-        verbose_name = "Пост"
-        verbose_name_plural = "Посты"
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
