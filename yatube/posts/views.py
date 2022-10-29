5757e52cdfee6965e3408f968fe29ca102906a35
Егор Архипцев <e.arkhiptsev@gmail.com>
2022-10-29 18:09:29 +0300

    fix after review 1.1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import PostForm
 from .models import Group, Post, User
